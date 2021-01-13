903c673310efa3bce59280a0af1082965e24fb59
sindresagen <71556636+sindresagen@users.noreply.github.com>
2021-01-13 13:47:04 +0100

    påbegynt oblig modul 2

---
+++ model.js
@@ -0,0 +1,13 @@
+const model = {
+    existing: [
+    { region: 'Bordeaux', castle: 'Chateau Dudon', year: 2010, price: 159.90 + 'kr' },
+    { region: 'Bordeaux', castle: 'Chateau Margaux', year: 2013, price: 5784.46 + 'kr' },
+    { region: 'Bordeaux', castle: 'Chateau Lafite Rothschild', year: 2016, price: 6300 + 'kr' },
+    { region: 'Some region', castle: 'Some castle', year: 'N/A', price: 'N/A' + 'kr' },
+    { region: 'Some region', castle: 'Some castle', year: 'N/A', price: 'N/A' + 'kr' },
+    ],
+
+    newNotes: [
+        {  },
+    ],
+}
---
+++ view.js
@@ -0,0 +1,1 @@
+let appDiv = document.getElementById('app');
