a25226e8b3c52616c3d3c5b1c7f40f1433fac4c9
sindresagen <71556636+sindresagen@users.noreply.github.com>
2021-01-15 12:21:15 +0100

    asdfhgd

--- model.js
+++ model.js
@@ -1,13 +1,19 @@
-const model = {
-    existing: [
-    { region: 'Bordeaux', castle: 'Chateau Dudon', year: 2010, price: 159.90 + 'kr' },
-    { region: 'Bordeaux', castle: 'Chateau Margaux', year: 2013, price: 5784.46 + 'kr' },
-    { region: 'Bordeaux', castle: 'Chateau Lafite Rothschild', year: 2016, price: 6300 + 'kr' },
-    { region: 'Some region', castle: 'Some castle', year: 'N/A', price: 'N/A' + 'kr' },
-    { region: 'Some region', castle: 'Some castle', year: 'N/A', price: 'N/A' + 'kr' },
-    ],
+// const model = {
+    
+//     existing: [
+//     { region: 'Bordeaux', castle: 'Chateau Dudon', year: 2002, price: 209.90 + 'kr' },
+//     { region: 'Bordeaux', castle: 'Chateau Margaux', year: 2013, price: 5784.46 + 'kr' },
+//     { region: 'Bordeaux', castle: 'Chateau Lafite Rothschild', year: 2016, price: 6300 + 'kr' },
+//     { region: 'Some region', castle: 'Some castle', year: 'N/A', price: 'N/A' + ' kr' },
+//     { region: 'Some region', castle: 'Some castle', year: 'N/A', price: 'N/A' + ' kr' },
+//     ],
 
-    newNotes: [
-        {  },
-    ],
-}
+//     newNotes: [
+//         {  },
+//     ],
+// };
+// for (let i = 0; i < model.existing.length; i++){
+//     if (model.existing[i].year == 'N/A') {
+//         console.log(model.existing[i]);
+//     }
+// };
--- view.js
+++ view.js
@@ -1,1 +1,35 @@
-let appDiv = document.getElementById('app');
+const model = {
+    
+    existing: [
+    { region: 'Bordeaux', castle: 'Chateau Dudon', year: 2002, price: 209.90 + 'kr' },
+    { region: 'Bordeaux', castle: 'Chateau Margaux', year: 2013, price: 5784.46 + 'kr' },
+    { region: 'Bordeaux', castle: 'Chateau Lafite Rothschild', year: 2016, price: 6300 + 'kr' },
+    { region: 'Some region', castle: 'Some castle', year: 'N/A', price: 'N/A' + ' kr' },
+    { region: 'Some region', castle: 'Some castle', year: 'N/A', price: 'N/A' + ' kr' },
+    ],
+
+    newNotes: [
+        // { region, castle, year, price: + ' kr' },
+        // {
+        //     regionTxt: '',
+        //     castleTxt: '',
+        //     year,
+        //     price,
+        //     mainNoteTxt: '',
+        //     sideNoteTxt: '',
+        // }
+    ],
+};
+
+updateView();
+function updateView() {
+
+
+}
+
+function listOfExistingWines() {
+    let vinListe = '';
+    for (let i = 0; i < model.existing.length; i++){
+        
+}
+}
