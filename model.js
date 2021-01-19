9bbb255eb21cbb8b3c8523d5fb9d85c3e9ebd3ba
sindresagen <71556636+sindresagen@users.noreply.github.com>
2021-01-19 15:02:06 +0100

    asdfghjjkkløø

--- model.js
+++ model.js
@@ -12,8 +12,31 @@
 //         {  },
 //     ],
 // };
+
+const appDiv = document.getElementById('app');
+const model = {
+
+    existing: [
+        { region: 'Bordeaux', castle: 'Chateau Dudon', year: 2002, price: 209.90 + 'kr' },
+        { region: 'Bordeaux', castle: 'Chateau Margaux', year: 2013, price: 5784.46 + 'kr' },
+        { region: 'Bordeaux', castle: 'Chateau Lafite Rothschild', year: 2016, price: 6300 + 'kr' },
+        { region: 'Some region', castle: 'Some castle', year: 'N/A', price: 'N/A' + ' kr' },
+        { region: 'Some region', castle: 'Some castle', year: 'N/A', price: 'N/A' + ' kr' },
+    ],
+
+    newNotes: [
+
+        {
+            regionTxt: '',
+            castleTxt: '',
+            year: '',
+            price: '',
+            mainNoteTxt: '',
+            sideNoteTxt: '',
+            notePicture: '',
+        }
+    ],
+};
 // for (let i = 0; i < model.existing.length; i++){
-//     if (model.existing[i].year == 'N/A') {
-//         console.log(model.existing[i]);
-//     }
+//     console.log(model.existing[i]);
 // };
--- view.js
+++ view.js
@@ -1,36 +1,67 @@
-const model = {
 
-    existing: [
-        { region: 'Bordeaux', castle: 'Chateau Dudon', year: 2002, price: 209.90 + 'kr' },
-        { region: 'Bordeaux', castle: 'Chateau Margaux', year: 2013, price: 5784.46 + 'kr' },
-        { region: 'Bordeaux', castle: 'Chateau Lafite Rothschild', year: 2016, price: 6300 + 'kr' },
-        { region: 'Some region', castle: 'Some castle', year: 'N/A', price: 'N/A' + ' kr' },
-        { region: 'Some region', castle: 'Some castle', year: 'N/A', price: 'N/A' + ' kr' },
-    ],
+listView();
+    function listView() {
+        listHTML = `
+        <table>
+        <tr>
+            <th>Region</th>
+            <th>Castle</th>
+            <th>Year</th>
+            <th>Price</th>    
+        </tr>
+        <td>Bordeaux</td>
+            <td>Chateau Dudon</td>
+            <td>2002</td>
+            <td>209.90</td>
+            <td><a class="userLink" onclick="userView()">Read</a></td>
+            <td><button class="editBtns">Edit</button></td>
+            <td><button class="editBtns">Add</button></td>
+            <td><button class="editBtns">X</button></td>
+        </table>
+        `;
+        appDiv.innerHTML = listHTML;
+    }
 
-    newNotes: [
+    function userView() {
+        userHTML = `<div class="backBtn"><a href="index.html">Back to admin page</a></div>
+        <h1>Chateau Dudon</h1>
+        <p class="sideNote">Some text some text some text some text <br /> some text some text
+            some text some text <br /> some text some text some text
+            some text <br /> some text some text some text some text
+            <br />
+            <br />
+            Some text some text some text some text <br /> some text some text
+            some text some text <br /> some text some text some text
+            some text <br /> some text some text some text some text</p>
 
-        {
-            regionTxt: '',
-            castleTxt: '',
-            year,
-            price,
-            mainNoteTxt: '',
-            sideNoteTxt: '',
-            notePicture: '',
-        }
-    ],
-};
+            <div><img id="bilde" src="oblig3.jpg"></div>
 
-updateView();
-function updateView() {
+        <p class="mainNote">APR 19 Ruby. Blueberries, light anise and cassis nose. 
+            Fresh acidity, ripe tannins, dark fruity, spicy, blueberries, good length. 85. 
+            Some text some text some text some text some text some text some text some text 
+            some text some text some text some text some text some text some text some text 
+            some text some text some text some text some text some text some text some text 
+            some text some text some text some text some text some text some text some text 
+            some text some text some text some text some text some text some text some text 
+            some text some text some text some text some text some text some text some text 
+            some text some text some text some text some text some text some text some text 
+            some text some text some text some text some text some text some text some text 
+            some text some text some text some text some text some text some text some text 
+            some text some text some text some text some text some text some text some text 
+            some text some text some text some text some text some text some text some text 
+            some text some text some text some text some text some text some text some text 
+            some text some text some text some text some text some text some text some text 
+            some text some text some text some text some text some text some text some text 
+            some text some text some text some text some text some text some text some text 
+            some text some text some text some text some text some text some text.</p>`;
 
+        appDiv.innerHTML = userHTML;
+    }
 
-}
 
 function listOfExistingWines() {
     let vinListe = '';
     for (let i = 0; i < model.existing.length; i++) {
-
+        console.log(model.existing[i]);
     }
 }
