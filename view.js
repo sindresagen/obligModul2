1511d9c16e2bbb69538e530e59e09cb1e54000b3
sindresagen <71556636+sindresagen@users.noreply.github.com>
2021-01-21 12:39:31 +0100

    Fått til loop

--- view.js
+++ view.js
@@ -2,28 +2,48 @@
 listView();
     function listView() {
         listHTML = `
-        <table>
-        <tr>
+        <table id="lists"></table>
+        `;
+        appDiv.innerHTML = listHTML;
+        const tableTag = document.getElementById('lists');
+        tableContent = `
+        <tr id="header">
+            <th><button class="addBtns" onclick="newReview()">+</button></th>
             <th>Region</th>
             <th>Castle</th>
             <th>Year</th>
-            <th>Price</th>    
+            <th>Price</th> 
+            <th>Link</th>
+            <th>Edit</th>
+            <th>Delete</th>
         </tr>
-        <td>Bordeaux</td>
-            <td>Chateau Dudon</td>
-            <td>2002</td>
-            <td>209.90</td>
+        `;
+
+        for (let i = 0; i < model.existing.length; i++) {
+             tableContent += existingWines(i);         
+        }
+        tableTag.innerHTML = tableContent;
+    }
+    
+    function existingWines(i){
+        const wines = model.existing[i];
+        return `
+        <tr id="existingRows">
+            <td>✔</td>
+            <td>${wines.region}</td>
+            <td>${wines.castle}</td>
+            <td>${wines.year}</td>
+            <td>${wines.price}</td>
             <td><a class="userLink" onclick="userView()">Read</a></td>
             <td><button class="editBtns">Edit</button></td>
-            <td><button class="editBtns">Add</button></td>
-            <td><button class="editBtns">X</button></td>
-        </table>
+            <td><button class="deleteBtns" onclick="deleteBtn()">X</button></td>
+        </tr>
         `;
-        appDiv.innerHTML = listHTML;
-    }
+    }    
+    
 
     function userView() {
-        userHTML = `<div class="backBtn"><a href="index.html">Back to admin page</a></div>
+        userHTML = `<div class="backBtn" onclick="listView()">Back to admin page</div>
         <h1>Chateau Dudon</h1>
         <p class="sideNote">Some text some text some text some text <br /> some text some text
             some text some text <br /> some text some text some text
@@ -58,10 +78,21 @@ listView();
         appDiv.innerHTML = userHTML;
     }
 
+    function newReview() {
+        newReviewHTML = `
+        <div class="backBtn" onclick="listView()">Back to admin page</div>
+        <h2>New Review:</h2>
+        <input id="regionInput" class="inputField" type="text" placeholder="Region"><button class="addInput">Add</button> <br />
+        <input id="castleInput" class="inputField" type="text" placeholder="Castle"><button class="addInput">Add</button> <br />
+        <input id="yearInput" class="inputField" type="text" placeholder="Year"><button class="addInput">Add</button> <br />
+        <input id="priceInput" class="inputField" type="text" placeholder="Price"><button class="addInput">Add</button> <br />
+        <input id="mainNote" type="text" placeholder="Main notes"><button class="addMainNote">Add</button>
+        <input id="sideNote" type="text" placeholder="Side note"><button class="addSideNote">Add</button>
+        <img id="imgPlaceholder" src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/e0/PlaceholderLC.png/600px-PlaceholderLC.png">
+        <button id="addImage">+</button>
+        `;
 
-function listOfExistingWines() {
-    let vinListe = '';
-    for (let i = 0; i < model.existing.length; i++) {
-        console.log(model.existing[i]);
+        appDiv.innerHTML = newReviewHTML;
     }
-}
+
+
